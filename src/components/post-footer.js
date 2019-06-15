4f7f60ec6d47282efec3ec5523546cfe486db245
Mike Robinson <mike.robinson.81@gmail.com>
2019-06-15 10:29:39 -0400

    Remove author from post footer

--- src/components/post-footer.js
+++ src/components/post-footer.js
@@ -5,8 +5,7 @@ export default function PostFooter({ post }) {
   return (
     <footer className="border-gray-200 border-t mt-8 pt-8 italic text-gray-500">
       <p>
-        Posted on <time>{post.publishDate}</time> by {post.author.name}. Tagged
-        in{" "}
+        Posted on <time>{post.publishDate}</time>. Tagged in{" "}
         {post.tags.map(tag => (
           <Link to={`/tags/${tag.slug}`}>{tag.name}</Link>
         ))}
